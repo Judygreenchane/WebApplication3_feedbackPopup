2f4798081d93fdac562b91355ad459f03bc0689a
Judygreenchan <chaneparambil893@gmail.com>
2024-10-31 16:21:32 +0100

    Add project files.

---
+++ WebApplication3_feedbackPopup/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication3_feedbackPopup.Models;
+
+namespace WebApplication3_feedbackPopup.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["FeedbackMessage"] = "Thank you for your feedback!";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+       
+
+    }
+}
---
+++ WebApplication3_feedbackPopup/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication3_feedbackPopup.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
